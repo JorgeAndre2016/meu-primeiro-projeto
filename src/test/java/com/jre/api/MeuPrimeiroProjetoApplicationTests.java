886695bc24fc44aab6aff017c86e0d7f5a327976
Jorge André <jorge_aquino@hotmail.com.br>
2021-02-16 14:50:00 -0300

    primeiro commit

---
+++ src/test/java/com/jre/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jre.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
