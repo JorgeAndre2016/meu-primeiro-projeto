a50cb3cda491850a3124d761e2e60d7681e056f2
Jorge André <jorge_aquino@hotmail.com.br>
2021-02-16 22:59:07 -0300

    gerando o hash de senhas com bcrypt

--- src/main/java/com/jre/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/jre/api/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,36 @@
 package com.jre.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jre.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	public int qtdPortPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por página = " + this.qtdPortPagina);
+			
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);
+			
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
 	}
 
 }
--- src/test/java/com/jre/api/MeuPrimeiroProjetoApplicationTests.java
+++ src/test/java/com/jre/api/MeuPrimeiroProjetoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jre.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MeuPrimeiroProjetoApplicationTests {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	public int qtdPorPagina;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void testarValorPaginacaoArquivoTeste() {
+		assertEquals(1000, this.qtdPorPagina);
 	}
 
 }
